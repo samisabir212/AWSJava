acba0899175ae0a00bcc2d068c3414ce3baa8411
Sami Sabir-Idrissi <admin@techteksolution.com>
2019-04-27 00:39:05 -0400

    updated

--- src/test/java/TestAWS/insertDynamoDB.java
+++ src/test/java/TestAWS/insertDynamoDB.java
@@ -1,12 +1,5 @@
 package TestAWS;
 
-import org.testng.annotations.Test;
-
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-
 public class insertDynamoDB {
 	
 	
@@ -15,19 +8,5 @@ public class insertDynamoDB {
 	 *
 	 */
 	
-	static AmazonDynamoDBClientBuilder client = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
-			new AwsClientBuilder.EndpointConfiguration("LocalHost", "us-east-2"));
-	
 
-	DynamoDB dynamoDB = new DynamoDB(client);
-
-	
-	@Test(enabled = true)
-	public void testInsertionToDynamoDB() {
-		
-		
-		
-		
-	}
-	
 }
